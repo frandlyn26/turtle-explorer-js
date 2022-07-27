30e2bc291b1b5f6d3e91d0d04d505b7168a017a2
frandlyn26 <91971276+frandlyn26@users.noreply.github.com>
2022-07-27 12:24:24 -0500

    update config with pts daemon info

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-var api = 'http://127.0.0.1:11898';
+var api = 'http://74.208.182.138:4243';
 var donationAddress = "";
-var blockTargetInterval = 30; // enter the block interval in seconds
-var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  100000000000000; // enter the total supply in atomic units
-var symbol = 'trtl'; // enter the coin's ticker
+var blockTargetInterval = 45; // enter the block interval in seconds
+var coinUnits = 1000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+var totalSupply =  1652000000000000; // enter the total supply in atomic units
+var symbol = 'P1652'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
